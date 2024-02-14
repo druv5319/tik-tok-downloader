0fc8401620c5b1963e86e707babbe9274c9ecd1f
Arrel Gray <arrel@withprimer.com>
2024-02-13 22:30:49 -0800

    DRY

--- index.js
+++ index.js
@@ -25,57 +25,26 @@ function deleteFile(fileName) {
   if (fs.existsSync(fileName)) fs.unlinkSync(fileName);
 }
 
-async function downloadCover(creator, id, url) {
-  const fileName = getFileName(creator, id, "jpg");
+async function downloadFile(creator, id, ext, url) {
+  const fileName = getFileName(creator, id, ext);
   if (getFileData(fileName)) {
-    console.log("cover already exists", creator, id);
+    console.log(`${ext} already exists`, creator, id);
     return;
   }
 
-  const coverStream = fs.createWriteStream(fileName);
-  axios({
-    url,
-    method: "GET",
-    responseType: "stream",
-  })
+  const stream = fs.createWriteStream(fileName);
+  axios({ url, method: "GET", responseType: "stream" })
     .then((response) => {
-      response.data.pipe(coverStream);
-      coverStream.on("finish", () => console.log("cover saved", creator, id));
-      coverStream.on("error", (error) => {
-        console.log("cover streaming error", id, error);
+      response.data.pipe(stream);
+      stream.on("finish", () => console.log(`${ext}  saved`, creator, id));
+      stream.on("error", (error) => {
+        console.log(`${ext}  stream error`, id, error);
         // delete the cover if it was partially saved
         deleteFile(fileName);
       });
     })
     .catch((error) => {
-      console.log("cover downloading error", id, error);
-    });
-}
-
-function downloadVideo(creator, id, url) {
-  const fileName = getFileName(creator, id, "mp4");
-  if (getFileData(fileName)) {
-    console.log("video already exists", creator, id);
-    return;
-  }
-
-  const videoStream = fs.createWriteStream(fileName);
-  axios({
-    url,
-    method: "GET",
-    responseType: "stream",
-  })
-    .then((response) => {
-      response.data.pipe(videoStream);
-      videoStream.on("finish", () => console.log("video saved", creator, id));
-      videoStream.on("error", (error) => {
-        console.log("video saving error", creator, id, error);
-        // delete the video if it was partially saved
-        deleteFile(fileName);
-      });
-    })
-    .catch((error) => {
-      console.log("video downloading error", id, error);
+      console.log(`${ext}  download error`, id, error);
     });
 }
 
@@ -87,10 +56,10 @@ async function fetchData(creator, id, videoUrl) {
   if (existingData) {
     try {
       const parsedData = JSON.parse(existingData);
-      console.log("data  already exists", creator, id);
+      console.log("data already exists", creator, id);
       return parsedData;
     } catch (err) {
-      console.log("data  parsing error", creator, id, err);
+      console.log("data parsing error", creator, id, err);
     }
   }
 
@@ -98,13 +67,13 @@ async function fetchData(creator, id, videoUrl) {
     const data = await tiktok.tiklydown(videoUrl);
     // make sure the data is good
     if (!data || !data.video || !data.video.noWatermark || !data.video.cover) {
-      throw new Error("data  format problem", creator, id, data);
+      throw new Error("data format problem", creator, id, data);
     }
     fs.writeFileSync(fileName, JSON.stringify(data, null, 2));
-    console.log("data  saved", creator, id);
+    console.log("data saved", creator, id);
     return data;
   } catch (err) {
-    console.log("data  fetching error", creator, id, err);
+    console.log("data fetching error", creator, id, err);
   }
 }
 
@@ -113,8 +82,8 @@ async function saveVideo(videoUrl) {
   const id = videoUrl.split("/").pop();
   const data = await fetchData(creator, id, videoUrl);
   if (data && data.video) {
-    downloadVideo(creator, id, data.video.noWatermark);
-    downloadCover(creator, id, data.video.cover);
+    downloadFile(creator, id, "mp4", data.video.noWatermark);
+    downloadFile(creator, id, "jpg", data.video.cover);
   } else {
     console.log("no data for", creator, id);
   }
